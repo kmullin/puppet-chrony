715993247a5b9cf904a94a1658ea4c657ab42155
Kevin Mullin <kevin@kpmullin.com>
2014-06-17 11:23:20 -0700

    WIP

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,26 +6,31 @@ class chrony::params {
   $chrony_password = 'xyzzy'
   $queryhosts = undef
 
+  $servers = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org',]
+  $config_keys_template = 'chrony/chrony.keys.erb'
+
   case $::osfamily {
     'Archlinux' : {
       $config = '/etc/chrony.conf'
       $config_template = 'chrony/chrony.conf.archlinux.erb'
       $config_keys = '/etc/chrony.keys'
-      $config_keys_template = 'chrony/chrony.keys.archlinux.erb'
       $package_name = ['chrony']
       $service_name = 'chrony'
-      $servers = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org',]
     }
     'RedHat' : {
       $config = '/etc/chrony.conf'
       $config_template = 'chrony/chrony.conf.redhat.erb'
       $config_keys = '/etc/chrony.keys'
-      $config_keys_template = 'chrony/chrony.keys.redhat.erb'
       $package_name = ['chrony']
       $service_name = 'chronyd'
-      $servers = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org',]
     }
-
+    'Debian': {
+      $config = '/etc/chrony/chrony.conf'
+      $config_template = 'chrony/chrony.conf.redhat.erb'
+      $config_keys = '/etc/chrony/chrony.keys'
+      $package_name = ['chrony']
+      $service_name = 'chrony'
+    }
     default     : {
       fail("The ${module_name} module is not supported
       on an ${::osfamily} based system.")
