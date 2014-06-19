6641dd58d9c659e208a27e4d84d8b319e7c6f6ec
Kevin Mullin <kevin@kpmullin.com>
2014-06-19 15:02:43 -0700

    verbose

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,5 @@
-class chrony::install (
-  $package_ensure = $chrony::package_ensure,
-  $package_name   = $chrony::package_name,) inherits chrony {
+class chrony::install inherits chrony {
+
   package { 'chrony':
     ensure => $package_ensure,
     name   => $package_name,
